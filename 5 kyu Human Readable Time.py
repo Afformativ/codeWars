6c18acacc9debaf046e5c1984d0c6527a2f887a9
Sasha Hrabar <40630611+Afformativ@users.noreply.github.com>
2023-02-06 03:16:40 -0500

    Create 5 kyu Human Readable Time.py

---
+++ 5 kyu Human Readable Time.py
@@ -0,0 +1,5 @@
+def make_readable(seconds):
+    hours = seconds // 3600
+    minutes = ( seconds % 3600 ) // 60
+    seconds = seconds % 60
+    return "{:02d}:{:02d}:{:02d}".format(hours, minutes, seconds)
